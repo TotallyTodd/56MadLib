042776ca78eb1ab261428d913bdd271ac3838ad9
56 lotus <joemilruiz@gmail.com>
2020-11-16 20:47:21 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    
+    Console.Write("Hi, Welcome to our MadLib!")
+
+    string title = "The Little Worm"
+  }
+} string story = Once upon a time, there lived a worm who dreamed of becoming a beautiful horse so that he could gallop in the grassy hills with his other horsey friends. One day he met a wizard frog who claimed he could grant his wish but with one condition. The worm had to travel from one side of the garden to the other to prove how determined he was on becoming a horse. The worm started on his journey and half way, a human approached from the house and started walking towards the worm. The worm stood proud and brave to hold his ground. The human then stood on him and the worm died. The end.
